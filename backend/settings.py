a1eb61f83e73e9912ac00403d9e634d3edeb9a2d
taiyebaafrin <taiyebasadia753@gmail.com>
2025-10-17 08:23:53 +0600

    ph2

--- backend/settings.py
+++ backend/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nfzm9aiuu1-hpi7l2i(x^+9817!l+*d!35dhd_k8cl@5zqw*qf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".vercel.app", "127.0.0.1", "https://phibook-backend.vercel.app"]
 
